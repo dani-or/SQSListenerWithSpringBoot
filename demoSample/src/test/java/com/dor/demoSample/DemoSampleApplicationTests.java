b8ad4bfcdf805cbc50227c463c91bb1980635955
Daniela Osorio R <danosori@bancolombia.com.co>
2020-07-15 12:05:21 -0500

    Se agrega versión inicial de sqs listener

---
+++ demoSample/src/test/java/com/dor/demoSample/DemoSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dor.demoSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
